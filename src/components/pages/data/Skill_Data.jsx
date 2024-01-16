d677e968af62172c34edd01df8460c85f8424c82
Ningsang Jabegu <86409265+Ningsang-Jabegu@users.noreply.github.com>
2024-01-16 13:15:58 +0545

    data of skill updated

--- src/components/pages/data/Skill_Data.jsx
+++ src/components/pages/data/Skill_Data.jsx
@@ -7,20 +7,33 @@ const skill_Data = {
         skillInfo: {
             "Started Date": {
                 "Year": 2024,
-                "Month": 1,
+                "Month": {
+                    "name": "Jan",
+                    "num" : 1
+                },
                 "Day": 1
             },
             "Completed Project": {
                 "Personal Website": 0,
                 "School Website": 0,
                 "Social Organization Website": 0,
-                "Commercial Store Website": 0
+                "Commercial Store Website": 0,
+                "Total" : function() {
+                    return(
+                        this["Personal Website"] + this["School Website"] + this["Social Organization Website"] + this["Commercial Store Website"]
+                    )
+                }
             },
             "Total Project To Complete": {
                 "Personal Website": 25,
                 "School Website": 25,
                 "Social Organization Website": 25,
-                "Commercial Store Website": 25
+                "Commercial Store Website": 25,
+                "Total" : function() {
+                    return(
+                        this["Personal Website"] + this["School Website"] + this["Social Organization Website"] + this["Commercial Store Website"]
+                    )
+                }
             },
             "Remaining Project": {
                 "Personal Website": function() {
@@ -34,11 +47,19 @@ const skill_Data = {
                 },
                 "Commercial Store Website": function() {
                     return skill_Data[skill_Data["Front-End Web Dev. Mentoring"].index].skillInfo["Total Project To Complete"]["Commercial Store Website"] - skill_Data[skill_Data["Front-End Web Dev. Mentoring"].index].skillInfo["Completed Project"]["Commercial Store Website"]
+                },
+                "Total" : function() {
+                    return(
+                        this["Personal Website"]() + this["School Website"]() + this["Social Organization Website"]() + this["Commercial Store Website"]()
+                    )
                 }
             },
             "Completion Date": {
                 "Year": 2025,
-                "Month": 1,
+                "Month": {
+                    "name": "Jan",
+                    "num" : 1
+                },
                 "Day": 1
             },
         },
@@ -51,38 +72,59 @@ const skill_Data = {
         skillInfo: {
             "Started Date": {
                 "Year": 2024,
-                "Month": 1,
+                "Month": {
+                    "name": "Jan",
+                    "num" : 1
+                },
                 "Day": 1
             },
             "Completed Project": {
                 "Personal Banner Design": 0,
                 "School Banner Design": 0,
                 "Social Organization Banner Design": 0,
-                "Commercial Store Banner Design": 0
+                "Commercial Store Banner Design": 0,
+                "Total" : function() {
+                    return(
+                        this["Personal Banner Design"] + this["School Banner Design"] + this["Social Organization Banner Design"] + this["Commercial Store Banner Design"]
+                    )
+                }
             },
             "Total Project To Complete": {
                 "Personal Banner Design": 25,
                 "School Banner Design": 25,
                 "Social Organization Banner Design": 25,
-                "Commercial Store Banner Design": 25
+                "Commercial Store Banner Design": 25,
+                "Total" : function() {
+                    return(
+                        this["Personal Banner Design"] + this["School Banner Design"] + this["Social Organization Banner Design"] + this["Commercial Store Banner Design"]
+                    )
+                }
             },
             "Remaining Project": {
-                "Personal Website": function() {
+                "Personal Banner Design": function() {
                     return skill_Data[skill_Data["Graphics Design"].index].skillInfo["Total Project To Complete"]["Personal Website"] - skill_Data[skill_Data["Graphics Design"].index].skillInfo["Completed Project"]["Personal Website"]
                 } ,
-                "School Website": function() {
+                "School Banner Design": function() {
                     return skill_Data[skill_Data["Graphics Design"].index].skillInfo["Total Project To Complete"]["School Website"] - skill_Data[skill_Data["Graphics Design"].index].skillInfo["Completed Project"]["School Website"]
                 },
-                "Social Organization Website": function() {
+                "Social Organization Banner Design": function() {
                     return skill_Data[skill_Data["Graphics Design"].index].skillInfo["Total Project To Complete"]["Social Organization Website"] - skill_Data[skill_Data["Graphics Design"].index].skillInfo["Completed Project"]["Social Organization Website"]
                 },
-                "Commercial Store Website": function() {
+                "Commercial Store Banner Design": function() {
                     return skill_Data[skill_Data["Graphics Design"].index].skillInfo["Total Project To Complete"]["Commercial Store Website"] - skill_Data[skill_Data["Graphics Design"].index].skillInfo["Completed Project"]["Commercial Store Website"]
+                },
+                "Total" : function() {
+                    return(
+                        this["Personal Banner Design"]() + this["School Banner Design"]() + this["Social Organization Banner Design"]() + this["Commercial Store Banner Design"]()
+                    )
                 }
             },
             "Completion Date": {
                 "Year": 2025,
-                "Month": 1,
+                "Month": {
+                    "name": "Jan",
+                    "num" : 1
+                },
                 "Day": 1
             },
         },
@@ -96,23 +138,44 @@ const skill_Data = {
         skillInfo: {
             "Started Date": {
                 "Year": 2024,
-                "Month": 1,
+                "Month": {
+                    "name": "Jan",
+                    "num" : 1
+                },
                 "Day": 1
             },
             "Completed Project": {
-                "Bootcamp at College" : 0
+                "Bootcamp at College" : 0,
+                "Total" : function() {
+                    return(
+                        this["Bootcamp at College"] 
+                    )
+                }
             },
             "Total Project To Complete": {
-                "Bootcamp at College" : 2
+                "Bootcamp at College" : 2,
+                "Total" : function() {
+                    return(
+                        this["Bootcamp at College"]
+                    )
+                }
             },
             "Remaining Project": {
-                "Personal Website": function() {
+                "Bootcamp at College": function() {
                     return skill_Data[skill_Data["Front-End Web Dev. Mentoring"].index].skillInfo["Total Project To Complete"]["Bootcamp at College"] - skill_Data[skill_Data["Front-End Web Dev. Mentoring"].index].skillInfo["Completed Project"]["Bootcamp at College"]
+                },
+                "Total" : function() {
+                    return(
+                        this["Bootcamp at College"]()
+                    )
                 }
             },
             "Completion Date": {
                 "Year": 2025,
-                "Month": 1,
+                "Month": {
+                    "name": "Jan",
+                    "num" : 1
+                },
                 "Day": 1
             },
         },
@@ -125,38 +188,59 @@ const skill_Data = {
         skillInfo: {
             "Started Date": {
                 "Year": 2024,
-                "Month": 1,
+                "Month": {
+                    "name": "Jan",
+                    "num" : 1
+                },
                 "Day": 1
             },
             "Completed Project": {
                 "Personal Wordpress Website": 0,
                 "School Wordpress Website": 0,
                 "Social Wordpress Organization Website": 0,
-                "Commercial Wordpress Store Website": 0
+                "Commercial Wordpress Store Website": 0,
+                "Total" : function() {
+                    return(
+                        this["Personal Wordpress Website"] + this["School Wordpress Website"] + this["Social Wordpress Organization Website"] + this["Commercial Wordpress Store Website"]
+                    )
+                }
             },
             "Total Project To Complete": {
                 "Personal Wordpress Website": 25,
                 "School Wordpress Website": 25,
                 "Social Wordpress Organization Website": 25,
-                "Commercial Wordpress Store Website": 25
+                "Commercial Wordpress Store Website": 25,
+                "Total" : function() {
+                    return(
+                        this["Personal Wordpress Website"] + this["School Wordpress Website"] + this["Social Wordpress Organization Website"] + this["Commercial Wordpress Store Website"]
+                    )
+                }
             },
             "Remaining Project": {
-                "Personal Website": function() {
+                "Personal Wordpress Website": function() {
                     return skill_Data[skill_Data["Wordpress"].index].skillInfo["Total Project To Complete"]["Personal Website"] - skill_Data[skill_Data["Wordpress"].index].skillInfo["Completed Project"]["Personal Website"]
                 } ,
-                "School Website": function() {
+                "School Wordpress Website": function() {
                     return skill_Data[skill_Data["Wordpress"].index].skillInfo["Total Project To Complete"]["School Website"] - skill_Data[skill_Data["Wordpress"].index].skillInfo["Completed Project"]["School Website"]
                 },
-                "Social Organization Website": function() {
+                "Social Wordpress Organization Website": function() {
                     return skill_Data[skill_Data["Wordpress"].index].skillInfo["Total Project To Complete"]["Social Organization Website"] - skill_Data[skill_Data["Wordpress"].index].skillInfo["Completed Project"]["Social Organization Website"]
                 },
-                "Commercial Store Website": function() {
+                "Commercial Wordpress Store Website": function() {
                     return skill_Data[skill_Data["Wordpress"].index].skillInfo["Total Project To Complete"]["Commercial Store Website"] - skill_Data[skill_Data["Wordpress"].index].skillInfo["Completed Project"]["Commercial Store Website"]
+                },
+                "Total" : function() {
+                    return(
+                        this["Personal Wordpress Website"]() + this["School Wordpress Website"]() + this["Social Wordpress Organization Website"]() + this["Commercial Wordpress Store Website"]()
+                    )
                 }
             },
             "Completion Date": {
                 "Year": 2025,
-                "Month": 1,
+                "Month": {
+                    "name": "Jan",
+                    "num" : 1
+                },
                 "Day": 1
             },
         },
--- src/components/pages/skills/HoldAllSkill.jsx
+++ src/components/pages/skills/HoldAllSkill.jsx
@@ -16,66 +16,9 @@ export default function HoldAllSkill({ select }) {
     const daysPassed = today.diff(startDate, 'days');
 
     const skillDetail = () => {
-        // return(<ul>
-        //     <br />
-        //     <li className="content-card">
-        //         <div className="title-wrapper">
-        //             <h5 className="h5">Started Date : <data value="2024-01-01">2024-Jan-1</data></h5>
-
-        //         </div>
-        //     </li><br />
-        //     <li className="content-card">
-        //         <div className="title-wrapper">
-        //             <h5 className="h5">Total Project to complete : <data value={totalProject}>{totalProject}</data></h5>
-
-        //         </div>
-        //     </li><br />
-        //     <li className="content-card">
-        //         <div className="title-wrapper">
-        //             <h5 className="h5">Started Date : <data value="2024-01-01">2024-Jan-1</data></h5>
-
-        //         </div>
-        //     </li><br />
-        //     <li className="content-card">
-        //         <div className="title-wrapper">
-        //             <h5 className="h5">Completed Project (include project of only this year) : </h5>
-        //             <data value="0">0%</data>
-        //         </div>
-
-        //         <div className="skill-progress-bg">
-        //             <div className="skill-progress-fill" style={{ width: "0%" }}></div>
-        //         </div>
-        //     </li><br />
-        //     <li className="content-card">
-        //         <div className="title-wrapper">
-        //             <h5 className="h5">Remaining Project : </h5>
-        //             <data value={totalProject - 0}>{totalProject - 0}</data>
-        //         </div>
-
-        //         <div className="skill-progress-bg">
-        //             <div className="skill-progress-fill" style={{ width: `${totalProject - 0}%` }}></div>
-        //         </div>
-        //     </li><br />
-        //     <li className="content-card">
-        //         <div className="title-wrapper">
-        //             <h5 className="h5">Time left : </h5>
-        //             <data value={totalDaysInYear - daysPassed}>{totalDaysInYear - daysPassed} days</data>
-        //         </div>
-
-        //         <div className="skill-progress-bg">
-        //             <div className="skill-progress-fill" style={{ width: `${100 - (daysPassed / totalDaysInYear) * 100}%` }}></div>
-        //         </div>
-        //     </li><br />
-        //     <li className="content-card">
-        //         <div className="title-wrapper">
-        //             <h5 className="h5">Completion Date : <data value="2025-01-01">2025-Jan-1</data></h5>
-
-        //         </div>
-        //     </li><br />
-        // </ul>)
-        <p>Skill detail</p>
+        return <p>Skill detail</p>
     }
-
+    // console.log(skill_Data["Web Design And Development"].skillInfo["Total Project To Complete"]["Total"]())
     const skillContent = Object.entries(skill_Data).map(([skillName, skill], index) => (
         <div className={`skill-main projects ${select !== skillName ? "hide" : ""}`}>
 
@@ -85,7 +28,56 @@ export default function HoldAllSkill({ select }) {
             <br />
             <p>{skill.skillText}</p>
             <p>I am planning to complete total of 100 ${select} project in the year 2024.</p>
-            {skillDetail}
+            <ul>
+            <br />
+            <li className="content-card">
+                <div className="title-wrapper">
+                    <h5 className="h5">Started Date : <data>{`${skill.skillInfo["Started Date"]["Year"]}-${skill.skillInfo["Started Date"]["Month"]["name"]}-${skill.skillInfo["Started Date"]["Day"]}`}</data></h5>
+                </div>
+            </li><br />
+            <li className="content-card">
+                <div className="title-wrapper">
+                    <h5 className="h5">Total Project to complete : <data>{skill.skillInfo["Total Project To Complete"]["Total"]()}</data></h5>
+
+                </div>
+            </li><br />
+            <li className="content-card">
+                <div className="title-wrapper">
+                    <h5 className="h5">Completed Project (include project of only this year) : </h5>
+                    <data >{skill.skillInfo["Completed Project"]["Total"]()}%</data>
+                </div>
+
+                <div className="skill-progress-bg">
+                    <div className="skill-progress-fill" style={{ width: `${skill.skillInfo["Completed Project"]["Total"]()}%` }}></div>
+                </div>
+            </li><br />
+            {/* <li className="content-card">
+                <div className="title-wrapper">
+                    <h5 className="h5">Remaining Project : </h5>
+                    <data >{skill.skillInfo["Remaining Project"]["Total"]()}</data>
+                </div>
+
+                <div className="skill-progress-bg">
+                    <div className="skill-progress-fill" style={{ width: `${skill.skillInfo["Remaining Project"]["Total"]()}%` }}></div>
+                </div>
+            </li><br /> */}
+            <li className="content-card">
+                <div className="title-wrapper">
+                    <h5 className="h5">Time left : </h5>
+                    <data value={totalDaysInYear - daysPassed}>{totalDaysInYear - daysPassed} days</data>
+                </div>
+
+                <div className="skill-progress-bg">
+                    <div className="skill-progress-fill" style={{ width: `${100 - (daysPassed / totalDaysInYear) * 100}%` }}></div>
+                </div>
+            </li><br />
+            <li className="content-card">
+                <div className="title-wrapper">
+                    <h5 className="h5">Completion Date : <data>{`${skill.skillInfo["Completion Date"]["Year"]}-${skill.skillInfo["Completion Date"]["Month"]["name"]}-${skill.skillInfo["Completion Date"]["Day"]}`}</data></h5>
+
+                </div>
+            </li><br />
+        </ul>
 
             <p>External link you might want to visit :</p>
 
